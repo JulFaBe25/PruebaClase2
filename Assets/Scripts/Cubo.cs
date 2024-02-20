457d529519cc949feacc13a7c867483227f7c626
Fabiandy <159928334+JulFaBe25@users.noreply.github.com>
2024-02-20 13:10:30 -0500

    Commit final 3
    Cambios de escenario, y sistema de puntaje

--- Assets/Scripts/Cubo.cs
+++ Assets/Scripts/Cubo.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cubo : MonoBehaviour
 
 {
-    public int Score;
+    public int puntos;
+    public Text textoScore;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        puntos = 0;
     }
 
     // Update is called once per frame
@@ -23,6 +25,7 @@ public class Cubo : MonoBehaviour
 
     private void OnTriggerEnter(Collider c){
         Debug.Log("Colision tipo trigger");
-        Score ++;
+        puntos ++;
+        textoScore.text = puntos.ToString();
     }
 }
