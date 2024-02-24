fcb50db791c5adb45987c7057991dd65ed5b93ee
JulFaBe25 <159928334+JulFaBe25@users.noreply.github.com>
2024-02-24 12:10:10 -0500

    Últimos cambios
    Modificación del escenario, cambios simples en cuanto a dinámica del juego.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public bool onGround;
-    public float ForceJump = 500;
+    public float ForceJump = 800;
     public float  fuerza_x;
+    public float incremento = 200f;
+    private bool teclaZpresionada = false;
+    private bool teclaXpresionada = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -33,21 +37,38 @@ public class BallController : MonoBehaviour
              Vector3 fuerza = new Vector3 (fuerza_x,ForceJump,0);
              GetComponent<Rigidbody>().AddForce(fuerza);
             }
-            
         }
 
          if (Input.GetKeyDown(KeyCode.Z))
         {
-            fuerza_x= fuerza_x-100;
+            teclaZpresionada= true;
         }
-
-         if (Input.GetKeyDown(KeyCode.X))
+        else if (Input.GetKeyUp(KeyCode.Z))
         {
-            fuerza_x= fuerza_x+100;
+            teclaZpresionada= false;
         }
+        
+        if (teclaZpresionada)
+        {
+            fuerza_x -= incremento* Time.deltaTime;
+        }
+        
+        
+        if (Input.GetKeyDown(KeyCode.X))
+       {
+           teclaXpresionada= true;
+       }
+       else if (Input.GetKeyUp(KeyCode.X))
+       {
+           teclaXpresionada= false;
+       }
+       
+       if (teclaXpresionada)
+       {
+           fuerza_x += incremento* Time.deltaTime;
+       }
     }
 
-    
     private void OnCollisionEnter(Collision c){
         Debug.Log("Colision");
         onGround = true;
